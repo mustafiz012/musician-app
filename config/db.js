452d027ea4bbd8d173c028aa3ffa20f7d7c40ebc
mustafiz012 <mustafiz012@outlook.com>
2020-05-02 12:18:11 +0600

    heroku database connection

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
 //musician: DB-name, postgres: user, BanglaDesh: user-password, localhost: host, postgres: DB
-module.exports = new Sequelize('musician', 'postgres', 'BanglaDesh', {
-    host: 'localhost',
+module.exports = new Sequelize('dfia4vj1b1lrc6', 'mwrydqyxitfwbd', 'ec55ae8ac265506808be30a0642156cfb78059124d4b3652075c5de485b741f2', {
+    host: 'ec2-50-17-178-87.compute-1.amazonaws.com',
     dialect: 'postgres',
 
     pool: {
